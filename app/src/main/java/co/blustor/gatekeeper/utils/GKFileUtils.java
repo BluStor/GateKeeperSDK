d6de0150191d57388321f4353ab3ed00e6371be1
Jeremy Neander <subjectego@gmail.com>
2016-01-21 11:56:14 -0600

    Add JavaDocs to 'utils' package classes

--- app/src/main/java/co/blustor/gatekeeper/utils/GKFileUtils.java
+++ app/src/main/java/co/blustor/gatekeeper/utils/GKFileUtils.java
@@ -4,7 +4,17 @@ import android.support.annotation.NonNull;
 
 import java.util.ArrayList;
 
+/**
+ * GKFileUtils is a functional, static class intended for common operations
+ * involving generic file attributes.
+ */
 public class GKFileUtils {
+    /**
+     * Join an array of {@link String} objects using the '/' path separator.
+     *
+     * @param paths the array of {@link String} objects to be joined
+     * @return the array of {@code paths} delimited by '/'
+     */
     public static String joinPath(String... paths) {
         ArrayList<String> list = nonblankPathSegments(paths);
         return GKStringUtils.join(list.toArray(new String[list.size()]), "/").replace("//", "/");
--- app/src/main/java/co/blustor/gatekeeper/utils/GKStringUtils.java
+++ app/src/main/java/co/blustor/gatekeeper/utils/GKStringUtils.java
@@ -1,8 +1,22 @@
 package co.blustor.gatekeeper.utils;
 
+/**
+ * GKStringUtils is a functional, static class intended for common operations
+ * involving {@code String} objects.
+ */
 public class GKStringUtils {
+    /**
+     * Join an array of {@link String} objects with a {@link String} separator.
+     *
+     * @param strings   the array of {@link String} objects to be joined
+     * @param separator the {@link String} object used to delimit the {@code strings}
+     * @return the array of {@code strings} delimited by the {@code separator}
+     * @since 0.5.0
+     */
     public static String join(String[] strings, String separator) {
-        if (strings.length == 0) return "";
+        if (strings.length == 0) {
+            return "";
+        }
 
         StringBuilder sb = new StringBuilder();
         sb.append(strings[0]);
