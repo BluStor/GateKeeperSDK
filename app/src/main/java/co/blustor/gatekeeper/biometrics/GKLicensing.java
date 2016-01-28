93f55d8bdfd8b31cc18ac6d178d6a5f7d1c1c66a
Jeremy Neander <subjectego@gmail.com>
2016-01-28 15:34:34 -0600

    Add JavaDocs to 'biometrics' package classes

--- app/src/main/java/co/blustor/gatekeeper/biometrics/GKLicensing.java
+++ app/src/main/java/co/blustor/gatekeeper/biometrics/GKLicensing.java
@@ -4,9 +4,15 @@ import com.neurotec.licensing.NLicense;
 
 import java.io.IOException;
 
+/**
+ * GKLicensing is responsible for obtaining the licenses necessary for GateKeeper biometrics.
+ */
 public class GKLicensing {
     public static final String TAG = GKLicensing.class.getSimpleName();
 
+    /**
+     * The {@code String} license IDs needed by GateKeeper biometrics.
+     */
     public static final String[] LICENSES = {
             "Biometrics.FaceExtraction",
             "Biometrics.FaceDetection",
@@ -16,11 +22,23 @@ public class GKLicensing {
     private final String sHostAddress;
     private final int sHostPort;
 
+    /**
+     * Create a {@code GKLicensing} with the given host address and port.
+     *
+     * @param hostAddress
+     * @param hostPort
+     * @since 0.5.0
+     */
     public GKLicensing(String hostAddress, int hostPort) {
         sHostAddress = hostAddress;
         sHostPort = hostPort;
     }
 
+    /**
+     * Obtain the licenses, making a network call if necessary.
+     *
+     * @since 0.5.0
+     */
     public void obtainLicenses() {
         for (String component : LICENSES) {
             try {
