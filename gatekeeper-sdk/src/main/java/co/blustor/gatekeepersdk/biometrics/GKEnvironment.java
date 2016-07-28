4fe8262f11ec25818300b98f3b3225b62d9db8b2
Mark W. Bennett <mbennett@blustor.co>
2016-07-28 02:30:42 -0500

    removed unused Mac address request method

--- gatekeeper-sdk/src/main/java/co/blustor/gatekeepersdk/biometrics/GKEnvironment.java
+++ gatekeeper-sdk/src/main/java/co/blustor/gatekeepersdk/biometrics/GKEnvironment.java
@@ -96,8 +96,6 @@ public class GKEnvironment {
 
     @NonNull
     private GKLicensing buildLicensing(Context context, GKFileActions fileActions) {
-
-        //String macAddress = getMacAddress(context);
         String macAddress = android.provider.Settings.Secure.getString(context.getContentResolver(), "bluetooth_address");
         Log.d(TAG, "buildLicensing(): macAddress = " + macAddress);
         BiometricLicenseManager licenseManager = new BiometricLicenseManager("/local", 5000);
@@ -109,12 +107,7 @@ public class GKEnvironment {
         WifiManager wifiService = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         WifiInfo connectionInfo = wifiService.getConnectionInfo();
         Log.d(TAG, "getMacAddress(): connectionInfo = " + connectionInfo);
-
-        String macAddress = android.provider.Settings.Secure.getString(context.getContentResolver(), "bluetooth_address");
-
         return connectionInfo == null ? null : connectionInfo.getMacAddress();
-
-
     }
 
     private void ensureDataFilesExist() {
